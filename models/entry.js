4d878399ac056663fb79fce90d67ad852a3e50a7
Jane Wong <jaynellew@gmail.com>
2016-05-31 14:40:41 -0700

    Refactored Entry model to refer to Prompt model and updated entries index page

--- models/entry.js
+++ models/entry.js
@@ -6,18 +6,38 @@ var autoIncrement = require('mongoose-auto-increment');
 autoIncrement.initialize(mongoose);
 
 var EntrySchema = new Schema({
-  prompt:     { type: Schema.Types.ObjectId, ref: 'Prompt' },
+  _prompt:    { type: mongoose.Schema.Types.ObjectId,
+                  ref: 'Prompt'
+                },
   body:        { type: String, trim: true, required: true},
-  created:     { type: Date, default: Date.now }
+  created:    { type: Date, default: Date.now }
 });
 
+// EntrySchema.methods.findPrompt = function(promptId) {
+//   // var promise = Prompt.findOne({ _id: promptId }).exec();
+//   //
+//   // promise.then(function(prompt) {
+//   //   console.log(prompt.prompt);
+//   //   if (prompt !== null) {
+//   //     res.return(prompt.prompt);
+//   //   }
+//   // // Prompt.findOne({ _id: promptId }, function(err, prompt){
+//   // //   if (prompt !== null) {
+//   // //     var promptBody = prompt.prompt;
+//   // //     console.log(prompt.prompt);
+//   // //   }
+//   // });
+//   this.model('Entry')
+//     .findOne({ _id: entryId })
+//     .populate('_prompt')
+//     .exec(function (err, entry) {
+//       if (err) return handleError(err);
+//
+//       Prompt.findOne({ _id: })
+//
+//       console.log(entry);
+//     });
+// }
+
 EntrySchema.plugin(autoIncrement.plugin, 'Entry');
-module.exports = mongoose.model("Entry", EntrySchema);
-
-// Populate entry with a
-Entry
-  .findOne({ _id: _ })
-  .populate('prompt')
-  .run(function (err, entry) {
-
-  });
+module.exports = mongoose.model('Entry', EntrySchema);
--- routes/entries.js
+++ routes/entries.js
@@ -5,57 +5,108 @@ var Prompt = require('../models/prompt');
 var Entry = require('../models/entry');
 
 router.get('/new', function(req, res, next) {
-  var promise = Prompt.count({}).exec();
+  // Grab a random prompt
+  Prompt.count().exec(function(err, count) {
+    var random = Math.floor(Math.random() * count);
 
-  promise.then(function(count) {
-    console.log(count);
-    console.log(Math.floor(Math.random() * count));
-    return Math.floor(Math.random() * count);
-  })
-  .then(function(randomId) {
-    Prompt.findOne({ _id: randomId }, function(err, prompt) {
+    var promise = Prompt.findOne({ _id: random }).exec();
+
+    promise.then(function(prompt) {
       console.log(prompt);
-      res.render('entries/new', { errors: [], prompt: prompt });
+      res.render(
+        'entries/new',
+        { errors: [], prompt: prompt }
+      );
     });
-  })
-  .catch(function(err) {
-    console.log('error:' + err);
   });
 });
 
-router.get('/', function(req, res, next) {
-  Entry.find({}, function(err, entries) {
-    if (err) {
-      next(err);
-    } else if (entries) {
-      res.render('entries/index', { entries: entries });
-    } else {
-      next(new Error("Failed to load entries."));
-    }
-  });
-});
+// router.get('/', function(req, res, next) {
+//   Entry.find({}, function(err, entries) {
+//     if (err) {
+//       next(err);
+//     } else if (entries) {
+//       res.render('entries/index', { entries: entries });
+//     } else {
+//       next(new Error("Failed to load entries."));
+//     }
+//   });
+// });
 
 router.post("/", function(req, res, next) {
-  var entry = new Entry({
-    prompt: req.body.prompt,
-    body: req.body.body
-  });
+  var promise = Prompt.findOne({ _id: req.body.promptId }).exec();
 
-  entry.save(function(err, entry) {
-    if (err) {
-      res.render('entries/new', { errors: err.errors });
-    } else {
-      console.log(entry);
-      res.redirect('/entries/');
-    }
-  });
+  promise.then(function(prompt) {
+    console.log(">>>>>>>>>>>>>>>>> PROMPT INSIDE PROMISE")
+    console.log(prompt);
+    console.log(">>>>>>>>>>>>>>>>> PROMPT ID INSIDE PROMISE")
+    console.log(prompt._id);
+
+    var entry = new Entry ({
+      body: req.body.body
+    });
+
+    entry._prompt = prompt;
+
+    console.log(">>>>>>>>>>>>>>>>> ENTRY RIGHT AFTER")
+    console.log(entry._prompt.prompt);
+
+    return entry;
+  }).then(function(entry){
+    console.log(">>>>>>>>>>>>>>>>> ENTRY AFTER THEN")
+    console.log(entry);
+    entry.save(function(err, entry) {
+      if (err) {
+        res.render('entries/new', { errors: err.errors });
+      } else {
+        console.log(">>>>>>>>>>>>>>>>> FINAL ENTRY")
+        console.log(entry);
+        res.redirect('/entries/');
+      }
+    });
+  })
+
+  // var prompt = Prompt.findOne({ _id: req.body.promptId }, function(err, prompt) {
+  //   console.log(">>>>>>>>>>>>>>>>> PROMPT")
+  //   console.log(prompt);
+  //   return prompt;
+  // });
+
+  // var promptRes = new PromptResponse({
+  //   prompt: prompt._id
+  // });
+  //
+  // promptRes.entries.push({ body: req.body.body })
+
+  // promptRes.save(function(err) {
+  //   if (err) {
+  //     res.render('entries/new', { errors: err.errors });
+  //   } else {
+  //     res.redirect('/entries/');
+  //   }
+  // });
+  //
+  // var entry = new Entry({
+  //   _prompt: prompt._id,
+  //   body: req.body.body
+  // });
+
+  // entry.save(function(err, entry) {
+  //   if (err) {
+  //     res.render('entries/new', { errors: err.errors });
+  //   } else {
+  //     console.log(">>>>>>>>>>>>>>>>> ENTRY")
+  //     console.log(entry);
+  //     res.redirect('/entries/');
+  //   }
+  // });
 });
 
 router.get('/:id'), function(req, res) {
   console.log("HELLO");
   console.log(req.body);
   console.log(req.params.id);
-  Entry.findOne({ _id: req.params.id }, function(err, entry) {
+  Entry.findOne({ _id: req.params._id }, function(err, entry) {
     console.log(entry);
     if (err) {
       res.end("Error");
@@ -66,15 +117,41 @@ router.get('/:id'), function(req, res) {
 };
 
 router.get('/', function(req, res, next) {
-  Entry.find({}, function(err, entries) {
-    if (err) {
-      next(err);
-    } else if (entries) {
-      res.render('entries/index', { entries: entries });
-    } else {
-      next(new Error("Failed to load entries."));
-    }
-  });
+
+  Entry.find({})
+        .populate('_prompt')
+        .exec(function(err, entries) {
+          console.log(entries);
+          res.render('entries/index', { entries: entries });
+        });
+
+  // Entry.find({}, function(err, entries) {
+  //   if (err) {
+  //     next(err);
+  //   } else if (entries) {
+  //     console.log(entries);
+  //     // for (var i = 0; i < entries.length; i++) {
+  //     //   Entry
+  //     //     .findOne({ _id: entries[i]})
+  //     // }
+  //     //
+  //     // Entry
+  //     //   .findOne({ _id: entryId })
+  //     //   .populate('_prompt')
+  //     //   .exec(function (err, entry) {
+  //     //     if (err) return handleError(err);
+  //     //
+  //     //     Prompt.findOne({ _id: })
+  //     //
+  //     //     console.log(entry);
+  //     //   });
+  //
+  //
+  //     res.render('entries/index', { entries: entries });
+  //   } else {
+  //     next(new Error("Failed to load entries."));
+  //   }
+  // });
 });
 
 module.exports = router;
